17e20191a3f99f7142e07412501de1590c5d2c83
mufaqar <mufaqar@gmail.com>
2023-05-15 14:51:17 +0500

    Footer

--- footer.php
+++ footer.php
@@ -11,25 +11,16 @@ $options = povash_WSH()->option();
 $allowed_html = wp_kses_allowed_html( 'post' );
 $icon_href = $options->get( 'image_favicon' ); 
 $page_id = ( $wp_query->is_posts_page ) ? $wp_query->queried_object->ID : get_the_ID();
-
-
-
 ?>
-
-
 <div class="clearfix"></div>
 <?php povash_template_load( 'templates/footer/footer.php', compact( 'page_id' ) );?>
 <?php if( $options->get( 'to_top' ) ):?>
-        <button class="scroll-top scroll-to-target" data-target="html">
-            <span class="fa fa-arrow-up"></span>
-        </button>
+<button class="scroll-top scroll-to-target" data-target="html">
+    <span class="fa fa-arrow-up"></span>
+</button>
 <?php endif; ?>
-
-
-
-
 </main>
-
 <?php wp_footer(); ?>
 </body>
+
 </html>
